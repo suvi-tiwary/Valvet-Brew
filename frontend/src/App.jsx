4eeb01c58498edaa9a12bcb1a4d254d27f80201d
suvi-tiwary <suvikumari345@gmail.com>
2026-03-12 21:29:12 +0530

    Velvet Brew

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </>
   )
 }
 
