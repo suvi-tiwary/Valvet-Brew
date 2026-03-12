6c34ac801b6a180fadac6a8ea522fb1831d333f7
suvi-tiwary <suvikumari345@gmail.com>
2026-03-12 21:51:52 +0530

    Velvet Brew 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/Home'
-
+import {Routes, Route} from "react-router-dom"
 const App = () => {
   return (
     <>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-green-100 w-full h-screen p-3'></div>
   )
 }
 
