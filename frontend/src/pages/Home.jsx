826fc22e202c032a002d42b714b6bb69354c68e5
suvi-tiwary <suvikumari345@gmail.com>
2026-03-15 00:19:28 +0530

    image

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { FaPizzaSlice } from "react-icons/fa";
+import backgroundImg from "/image.jpg"
 
 const Home = () => {
   return (
-    <div className='bg-green-100 w-full h-screen pt-[65px]'>
-        <FaPizzaSlice />
+    <div className='bg-green-100 w-full min-h-screen mt-[65px]'>
+       <img src={backgroundImg} alt="" className='w-full h-[70vh]' />
+       <h1 className='flex justify-center text-[45px]'>Welcome to Cafeteria of the House</h1>
+       
     </div>
   )
 }
