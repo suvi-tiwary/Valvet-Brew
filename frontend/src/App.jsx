19444c825d7eba6bb6bb516e8126117f18920ffb
suvi-tiwary <suvikumari345@gmail.com>
2026-03-16 20:21:45 +0530

    Velvet-brew

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import Home from './pages/Home'
-import {Routes, Route} from "react-router-dom"
-import Navbar from './componenets/Navbar'
-import Signup from './pages/Signup'
+
 const App = () => {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-    </Routes>
-    </>
+    <div>App</div>
   )
 }
 
